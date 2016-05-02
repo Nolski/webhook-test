e15371bb0358bf7ef2abe698b2a750c587bfcf85
Mike Nolan <me@nolski.rocks>
2016-05-01 22:49:20 -0400

    created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='webhooks',
+      version='1.0',
+      description='just an app',
+      author='me',
+      author_email='me@michael-nolan.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask'],
+)
